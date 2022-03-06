7e16e33b45030917ad9eb67a343d52334dcde937
1-UP Nuke <65999898+1UPNuke@users.noreply.github.com>
2022-03-06 22:12:19 +0200

    Removed toast used for debugging

--- app/src/main/java/fi/metropolia/valemajoneesi/moodprint/EmotionAdapter.java
+++ app/src/main/java/fi/metropolia/valemajoneesi/moodprint/EmotionAdapter.java
@@ -2,13 +2,11 @@ package fi.metropolia.valemajoneesi.moodprint;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.core.widget.ImageViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
@@ -80,7 +78,6 @@ public class EmotionAdapter extends RecyclerView.Adapter<EmotionAdapter.ViewHold
                     EmotionTracker.getInstance().unselectEmotion(i);
                     ImageViewCompat.setImageTintList((ImageView) view, ColorStateList.valueOf(Color.rgb(128, 128, 128)));
                 }
-                Toast.makeText(view.getContext(), EmotionTracker.getInstance().getSelected().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
