d4adb5b1a7275930aec45e34d9be8a9172520e41
1-UP Nuke <65999898+1UPNuke@users.noreply.github.com>
2022-03-08 12:38:49 +0200

    Finished HistoryActivity in time for the presentation

--- app/src/main/java/fi/metropolia/valemajoneesi/moodprint/HistoryActivity.java
+++ app/src/main/java/fi/metropolia/valemajoneesi/moodprint/HistoryActivity.java
@@ -3,20 +3,31 @@ package fi.metropolia.valemajoneesi.moodprint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class HistoryActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle back button
         switch (item.getItemId()) {
             case android.R.id.home:
-                EmotionTracker.getInstance().unselectAll();
+                EmotionTracker.unselectAll();
                 finish();
                 return true;
         }
@@ -28,22 +39,29 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        EmotionTracker.setInstance(this);
+        EmotionTracker.initialize(this);
 
+        // Set back button
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        Collection<List> emotionListCollection = EmotionTracker.getInstance().getHistory().values();
+        List<String> emoStringList = new ArrayList<>();
+        for(Map.Entry entr : EmotionTracker.getHistory().entrySet()) {
+            String str = LocalDateTime.ofInstant(
+                    Instant.ofEpochSecond((long)entr.getKey()),
+                    TimeZone.getDefault().toZoneId()
+            ).toString()+": ";
+
+            for (Emotion emo : (List<Emotion>) entr.getValue()) {
+                str += emo.getName() + " ";
+            }
+            emoStringList.add(str);
+        }
+
+        Collections.reverse(emoStringList);
 
         ListView lv = (ListView) findViewById(R.id.listView);
 
-        for(List<Emotion> surveyAnswer : emotionListCollection) {
-            //A list of emotions chosen in a survey
-            for(Emotion emotion : surveyAnswer) {
-                //Individual emotion
-                //Get name of emotion
-                String emoName = emotion.getName();
-            }
-        }
+        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, emoStringList));
     }
 }
